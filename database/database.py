51274f058a6867c64aba7577238cb0c68094f064
Benjamin Campos <benjamin.campos@neering.cl>
2024-06-15 01:28:50 -0400

    UPDATE se añade env DB_NAME

--- database/database.py
+++ database/database.py
@@ -9,10 +9,11 @@ load_dotenv()
 DB_ENGINE=os.getenv("DB_ENGINE")
 DB_HOST=os.getenv("DB_HOST")
 DB_PORT=os.getenv("DB_PORT")
+DB_NAME=os.getenv("DB_NAME")
 DB_USER=os.getenv("DB_USER")
 DB_PASSWORD=os.getenv("DB_PASSWORD")
 
-SQLALCHEMY_DATABASE_URL = f"{DB_ENGINE}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/db"
+SQLALCHEMY_DATABASE_URL = f"{DB_ENGINE}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
