3dd71a9c0bdc12e6bd9e0d541ed29ab711455134
nrojel <nicole.rojelg@gmail.com>
2024-06-26 16:28:26 -0400

    FIX: cors

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.recipe import recipe
 from routes.daymenu import daymenu
 from routes.menu import menu
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(recipe)
 app.include_router(daymenu)
 app.include_router(menu)
