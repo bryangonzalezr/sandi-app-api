f2f8d3701ea147a3b2dc8832c658581fdc11a4cf
Benjamín Campos <benjamin.camposc@usm.cl>
2024-08-26 00:27:39 -0400

    UPDATE se añaden rutas usuarios,menus y recetas

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\MenuController;
+use App\Http\Controllers\Api\RecipeController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('usuarios', UserController::class);
+    Route::apiResource('menus', MenuController::class);
+    Route::apiResource('receta', RecipeController::class)->except('index');
+    Route::get('recetas', [RecipeController::class, 'index']);
+    Route::get('receta/api/', [RecipeController::class, 'getRecipeFromApi']);
 });
