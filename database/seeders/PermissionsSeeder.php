ec078b2c4e4a8b22a850f0e1b3dd21199af4cfa6
Benjamín Campos <benjamin.camposc@usm.cl>
2024-08-27 11:57:28 -0400

    UPDATE permisos de usuario

--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -76,52 +76,164 @@ class PermissionsSeeder extends Seeder
         ];
     }
 
-    /* protected function seedPermissions(): void
+    protected function seedPermissions(): void
     {
+        //Permisos plan nutricional
+
+        //Nutricionista
         Permission::create([
             'name' => 'nutritional_plan.create',
             'description' => 'Usuario puede crear un Plan Nutricional'
         ]);
         Permission::create([
-            'name' => 'nutritional_plan.view_any',
+            'name' => 'nutritional_plan.view',
             'description' => 'Usuario puede ver información de cualquier Plan Nutricional'
         ]);
         Permission::create([
-            'name' => 'nutritional_plan.update_any',
+            'name' => 'nutritional_plan.update',
             'description' => 'Usuario puede actualizar información de cualquier Plan Nutricional'
         ]);
         Permission::create([
-            'name' => 'nutritional_plan.delete_any',
-            'description' => 'Usuario puede eliminar cualquier Plan Nutricional'
+            'name' => 'nutritional_plan.delete',
+            'description' => 'Usuario puede archivar cualquier Plan Nutricional'
         ]);
+
+        //Paciente
         Permission::create([
             'name' => 'nutritional_plan.view_own',
-            'description' => 'Usuario puede ver información de su Plan Nutricional'
+            'description' => 'Usuario puede ver información de su Plan Nutricional actual'
         ]);
+
+        //Permisos perfil nutricional
+
+        //Nutricionista
+        Permission::create([
+            'name' => 'nutritional_profile.create',
+            'description' => 'Usuario puede crear un Perfil Nutricional'
+        ]);
+        Permission::create([
+            'name' => 'nutritional_profile.view',
+            'description' => 'Usuario puede ver información de cualquier Perfil Nutricional'
+        ]);
+        Permission::create([
+            'name' => 'nutritional_profile.update',
+            'description' => 'Usuario puede actualizar información de cualquier Plan Nutricional'
+        ]);
+        Permission::create([
+            'name' => 'nutritional_profile.delete',
+            'description' => 'Usuario puede eliminar cualquier Perfil Nutricional'
+        ]);
+
+        //Paciente
+        Permission::create([
+            'name' => 'nutritional_profile.view_own',
+            'description' => 'Usuario puede ver información de su Perfil Nutricional'
+        ]);
+
+        //Permisos recetas
+
+         //Nutricionista y Paciente
+         Permission::create([
+            'name' => 'recipe.create',
+            'description' => 'Usuario puede crear una Receta'
+        ]);
+        Permission::create([
+            'name' => 'recipe.view',
+            'description' => 'Usuario puede ver información de sus recetas'
+        ]);
+        Permission::create([
+            'name' => 'recipe.update',
+            'description' => 'Usuario puede actualizar información de sus recetas'
+        ]);
+        Permission::create([
+            'name' => 'recipe.delete',
+            'description' => 'Usuario puede eliminar sus recetas'
+        ]);
+
+        //Paciente y Usuario Básico
+        Permission::create([
+            'name' => 'recipe.generate',
+            'description' => 'Usuario puede generar recetas con el asistente'
+        ]);
+
+        //Nutricionista
+        Permission::create([
+            'name' => 'recipe.view_any',
+            'description' => 'Usuario puede ver información de las recetas de sus pacientes'
+        ]);
+
+        //Permisos menus
+
+        //Nutricionista
+        Permission::create([
+            'name' => 'menu.update',
+            'description' => 'Usuario puede actualizar información de sus Menús guardados'
+        ]);
+
+        //Nutricionista y Paciente
+        Permission::create([
+            'name' => 'menu.create',
+            'description' => 'Usuario puede crear un Menú Diario, Semanal o Mensual'
+        ]);
+        Permission::create([
+            'name' => 'menu.view',
+            'description' => 'Usuario puede ver información de sus Menús guardados'
+        ]);
+        Permission::create([
+            'name' => 'menu.delete',
+            'description' => 'Usuario puede eliminar sus Menús guardados'
+        ]);
+        Permission::create([
+            'name' => 'menu.generate',
+            'description' => 'Usuario puede generar Menús con el asistente'
+        ]);
+
+        //Permisos suscripción
+
+        //Nutricionista
+        Permission::create([
+            'name' => 'suscription.view',
+            'description' => 'Usuario puede ver información de sus suscripciones'
+        ]);
+        Permission::create([
+            'name' => 'suscription.create',
+            'description' => 'Usuario puede crear una suscripción'
+        ]);
+        Permission::create([
+            'name' => 'suscription.delete',
+            'description' => 'Usuario puede cancelar suscricpción'
+        ]);
+
+        //Permisos usuarios
+
+        //SuperAdmin
         Permission::create([
             'name' => 'users.create',
             'description' => 'Usuario puede crear un Usuario'
         ]);
+
+        //Nutricionista, Paciente y Usuario Básico
         Permission::create([
-            'name' => 'users.view_any',
-            'description' => 'Usuario puede ver información de cualquier Usuario'
+            'name' => 'users.view_own',
+            'description' => 'Usuario puede ver su información '
         ]);
         Permission::create([
-            'name' => 'users.update_any',
-            'description' => 'Usuario puede actualizar información de cualquier Usuario'
+            'name' => 'users.update_own',
+            'description' => 'Usuario puede actualizar su información'
         ]);
-        Permission::create([
-            'name' => 'users.delete_any',
-            'description' => 'Usuario puede eliminar cualquier Usuario'
-        ]);
+
+        // Permisos pacientes asociados
+
+        //Nutricionista
         Permission::create([
             'name' => 'users.view_patient_users',
             'description' => 'Usuario puede ver información de sus Usuarios Pacientes'
         ]);
         Permission::create([
             'name' => 'users.delete_patient_users',
-            'description' => 'Usuario puede eliminar sus Usuarios Pacientes'
+            'description' => 'Usuario puede desvincularse de sus Usuarios Pacientes'
         ]);
+
         Permission::create([
             'name' => 'roles.assign_patient_user',
             'description' => 'Usuario puede asignar Rol Paciente'
@@ -130,5 +242,31 @@ class PermissionsSeeder extends Seeder
             'name' => 'roles.retract_patient_user',
             'description' => 'Usuario puede desasignar Rol Paciente'
         ]);
-    } */
+
+        //Permisos tarjeatas de contacto
+
+        //Nutricionista
+        Permission::create([
+            'name' => 'contact_cards.create',
+            'description' => 'Usuario puede crear una tarjeta de contacto'
+        ]);
+
+        Permission::create([
+            'name' => 'contact_cards.update',
+            'description' => 'Usuario puede actualizar información de su tarjeta de contacto'
+        ]);
+
+        Permission::create([
+            'name' => 'contact_cards.delete',
+            'description' => 'Usuario puede eliminar sus tarjetas de contacto'
+        ]);
+
+        //Nutricionista y Usuario Básico
+        Permission::create([
+            'name' => 'contact_cards.view',
+            'description' => 'Usuario puede ver información de una tarjeta de contacto'
+        ]);
+
+
+    }
 }
