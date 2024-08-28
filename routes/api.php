3c07533af30ef1c7d33c2155fb024eec12667b47
Benjamín Campos <benjamin.camposc@usm.cl>
2024-08-28 09:46:05 -0400

    ADD rutas restantes

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,12 @@
 
 use App\Http\Controllers\Api\DayMenuController;
 use App\Http\Controllers\Api\MenuController;
+use App\Http\Controllers\Api\NutritionalPlanController;
+use App\Http\Controllers\Api\NutritionalProfileController;
+use App\Http\Controllers\Api\ProgressController;
 use App\Http\Controllers\Api\RecipeController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\VisitController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->group(function () {
@@ -15,7 +19,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     //Rutas Recetas
     Route::apiResource('receta', RecipeController::class)->except('index');
     Route::get('recetas', [RecipeController::class, 'index']);
-    Route::post('receta/eda', [RecipeController::class, 'getRecipeFromApi']);
+    Route::post('receta/generar', [RecipeController::class, 'getRecipeFromApi']);
 
     //Rutas DayMenus
     Route::apiResource('menu-diario', DayMenuController::class);
@@ -25,6 +29,19 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('menu', MenuController::class);
     Route::post('menu/generar', [MenuController::class, 'generateMenu']);
 
+    //Rutas Perfiles Nutricionales
+    Route::apiResource('perfil-nutricional', NutritionalProfileController::class);
 
+    //Rutas Pacientes
+    Route::apiResource('paciente', UserController::class);
+
+    //Rutas Planes Nutricionales
+    Route::apiResource('plan-nutricional', NutritionalPlanController::class);
+
+    //Rutas Progreso
+    Route::apiResource('progreso', ProgressController::class);
+
+    //Rutas de Consultas
+    Route::apiResource('consulta', VisitController::class);
 
 });
