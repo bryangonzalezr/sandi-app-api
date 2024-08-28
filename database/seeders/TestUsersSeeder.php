416bc2e9e5fc133ba9525fd277394e23f3c0625a
Benjamín Campos <benjamin.camposc@usm.cl>
2024-08-28 01:47:56 -0400

    UPDATE test users

--- database/seeders/TestUsersSeeder.php
+++ database/seeders/TestUsersSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\NutritionalPlan;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -22,7 +23,7 @@ class TestUsersSeeder extends Seeder
             'birthdate' => $birthdate,
             'age' => Carbon::parse($birthdate)->age,
             'phone_number' => '989220812',
-            'civil_status' => 'Casada',
+            'civil_status' => 'Casado(a)',
             'description' => '',
             'objectives' => '',
             'email' => 'belen@test.cl',
@@ -39,13 +40,126 @@ class TestUsersSeeder extends Seeder
             'birthdate' => $birthdate,
             'age' => Carbon::parse($birthdate)->age,
             'phone_number' => '911112222',
-            'civil_status' => 'Casada',
+            'civil_status' => 'Casado(a)',
             'description' => '',
             'objectives' => 'Bajar de peso',
             'email' => 'claudia@test.cl',
             'password' => bcrypt('sandi.,2024'),
         ]);
+        $user->assignRole('paciente');
+
+        $nutritional_plan = NutritionalPlan::create([
+            'patient_id' => $user->id,
+            'height' => 1.63,
+            'weight' => 76,
+            'physical_activity' => [
+                'actividad' => 'No',
+                'tiempo' => [
+                    'cantidad' => 7,
+                    'unidad' => 'años'
+                ],
+            ],
+            'habits' => [
+                'alcohol' => [
+                    'actividad' => 'Si',
+                    'comentario' => 'Tomo una copa de vino ocasionalmente con amigas',
+                ],
+                'tabaco'  => [
+                    'actividad' => 'Si',
+                    'comentario' => 'Fumo 5 cigarros al día',
+                ],
+            ],
+            'allergies' => [
+                'alergia' => 'Si',
+                'alergias' => [
+                    'Maní',
+                    'Frutos secos',
+                    'Mariscos',
+                ],
+            ],
+            'intolerances' => [
+                'intolerancia' => 'Si',
+                'intolerancias' => [
+                    'Carnes rojas',
+                ],
+            ],
+            'family_antecedents' => [],
+            'subjective_assessment' => [],
+            'nutritional_anamnesis' => [
+                'plan_anterior' => 'No',
+                'agua' => [
+                    'toma_agua' => 'Si',
+                    'cantidad' => 'Baja',
+                ],
+                'observaciones' => []
+            ],
+        ]);
+
+        $birthdate = Carbon::createFromFormat('Y-m-d', '2000-11-08');
+        $user = User::create([
+            'name' => 'John',
+            'last_name' => 'Barriga',
+            'sex' => 'Masculino',
+            'birthdate' => $birthdate,
+            'age' => Carbon::parse($birthdate)->age,
+            'phone_number' => '911112222',
+            'civil_status' => 'Soltero(a)',
+            'description' => '',
+            'objectives' => 'Bajar de peso e intentar ganar musculos',
+            'email' => 'claudia@test.cl',
+            'password' => bcrypt('sandi.,2024'),
+        ]);
 
         $user->assignRole('paciente');
+
+        $nutritional_plan = NutritionalPlan::create([
+            'patient_id' => $user->id,
+            'height' => 1.72,
+            'weight' => 92,
+            'physical_activity' => [
+                'actividad' => 'No',
+                'tiempo' => [
+                    'cantidad' => 2,
+                    'unidad' => 'años'
+                ],
+            ],
+            'habits' => [
+                'alcohol' => [
+                    'actividad' => 'No',
+                    'comentario' => 'No tomo alcohol',
+                ],
+                'tabaco'  => [
+                    'actividad' => 'Si',
+                    'comentario' => 'Fumador social',
+                ],
+            ],
+            'allergies' => [
+                'alergia' => 'Si',
+                'alergias' => [
+                    'Maní',
+                    'Frutos secos',
+                    'Mariscos',
+                ],
+            ],
+            'intolerances' => [
+                'intolerancia' => 'Si',
+                'intolerancias' => [
+                    'Lactosa',
+                    'Mariscos',
+                    'Carnes',
+                ],
+            ],
+            'family_antecedents' => [],
+            'subjective_assessment' => [],
+            'nutritional_anamnesis' => [
+                'plan_anterior' => 'No',
+                'agua' => [
+                    'toma_agua' => 'Si',
+                    'cantidad' => 'Alta',
+                ],
+                'observaciones' => [],
+            ],
+        ]);
+
     }
 }
