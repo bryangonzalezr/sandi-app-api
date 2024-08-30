272376e61cdca0684269fcf12f262ea99d04abc7
Benjamín Campos <benjamin.camposc@usm.cl>
2024-08-29 23:38:52 -0400

    FIX api routes

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,81 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [LoginController::class, 'apiLogin']);
 
+/* Route::middleware(['auth:sanctum'])->group(function () {
+
+    //Rutas Usuarios
+    Route::get('usuarios', [UserController::class, 'index']);
+    Route::get('usuario/{user}', [UserController::class, 'show']);
+    Route::put('usuario/{user}', [UserController::class, 'update']);
+    Route::delete('usuario/{user}', [UserController::class, 'destroy']);
+    Route::get('usuario/{user}/perfil-nutricional', [UserController::class, 'nutritionalProfile']);
+    Route::get('roles', [UserController::class, 'roleList']);
+
+    //Rutas Recetas
+    Route::get('recetas', [RecipeController::class, 'index']);
+    Route::get('receta/{recipe}', [RecipeController::class, 'show']);
+    Route::post('receta', [RecipeController::class, 'store']);
+    Route::put('receta/{recipe}', [RecipeController::class, 'update']);
+    Route::delete('receta/{recipe}', [RecipeController::class, 'delete']);
+    Route::post('receta/generar', [RecipeController::class, 'getRecipeFromApi']);
+
+    //Rutas DayMenus
+    Route::get('menus-diarios', [DayMenuController::class, 'index']);
+    Route::get('menu-diario/{dayMenu}', [DayMenuController::class, 'show']);
+    Route::post('menu-diario', [DayMenuController::class, 'store']);
+    Route::put('menu-diario/{dayMenu}', [DayMenuController::class, 'update']);
+    Route::delete('menu-diario/{dayMenu}', [DayMenuController::class, 'delete']);
+    Route::post('menu-diario/generar', [DayMenuController::class, 'generateDayMenu']);
+
+    //Rutas Menus
+    Route::apiResource('menu', MenuController::class)->except('index');
+    Route::get('menus', [MenuController::class, 'index']);
+    Route::post('menu/generar', [MenuController::class, 'generateMenu']);
+
+    //Rutas Perfiles Nutricionales
+    Route::get('perfiles-nutricionales', [NutritionalProfileController::class, 'index']);
+    Route::get('perfil-nutricional/{nutritionalProfile}', [NutritionalProfileController::class, 'show']);
+    Route::post('perfil-nutricional', [NutritionalProfileController::class, 'store']);
+    Route::put('perfil-nutricional/{nutritionalProfile}', [NutritionalProfileController::class, 'update']);
+    Route::delete('perfil-nutricional/{nutritionalProfile}', [NutritionalProfileController::class, 'delete']);
+
+    //Rutas Pacientes
+    Route::get('pacientes', [PatientController::class, 'index']);
+    Route::get('paciente/{patient}', [PatientController::class, 'show']);
+    Route::post('paciente', [PatientController::class, 'store']);
+    Route::put('paciente/{patient}', [PatientController::class, 'update']);
+    Route::delete('paciente/{patient}', [PatientController::class, 'delete']);
+
+    //Rutas Planes Nutricionales
+    Route::get('planes-nutricionales', [NutritionalPlanController::class, 'index']);
+    Route::get('plan-nutricional/{nutritionalPlan}', [NutritionalPlanController::class, 'show']);
+    Route::post('plan-nutricional', [NutritionalPlanController::class, 'store']);
+    Route::put('plan-nutricional/{nutritionalPlan}', [NutritionalPlanController::class, 'update']);
+    Route::delete('plan-nutricional/{nutritionalPlan}', [NutritionalPlanController::class, 'delete']);
+
+    //Rutas Progreso
+    Route::get('progresos', [ProgressController::class, 'index']);
+    Route::get('progreso/{progress}', [ProgressController::class, 'show']);
+    Route::post('progreso', [ProgressController::class, 'store']);
+    Route::put('progreso/{progress}', [ProgressController::class, 'update']);
+    Route::delete('progreso/{progress}', [ProgressController::class, 'delete']);
+
+    //Rutas de Consultas
+    Route::get('consultas', [VisitController::class, 'index']);
+    Route::get('consulta/{visit}', [VisitController::class, 'show']);
+    Route::post('consulta', [VisitController::class, 'store']);
+    Route::put('consulta/{visit}', [VisitController::class, 'update']);
+    Route::delete('consulta/{visit}', [VisitController::class, 'delete']);
+
+    //Rutas Tarjetas de contacto
+    Route::get('tarjetas', [ContactCardController::class, 'index']);
+    Route::get('tarjeta/{contactCard}', [ContactCardController::class, 'show']);
+    Route::post('tarjeta', [ContactCardController::class, 'store']);
+    Route::put('tarjeta/{contactCard}', [ContactCardController::class, 'update']);
+    Route::delete('tarjeta/{contactCard}', [ContactCardController::class, 'delete']);
+
+}); */
+
 Route::middleware(['auth:sanctum'])->group(function () {
 
     //Rutas Usuarios
@@ -48,7 +123,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('menu/generar', [MenuController::class, 'generateMenu']);
 
     //Rutas Perfiles Nutricionales
-    Route::get('perfil-nutricional', [NutritionalProfileController::class, 'index']);
+    Route::get('perfiles-nutricionales', [NutritionalProfileController::class, 'index']);
     Route::get('perfil-nutricional/{nutritionalProfile}', [NutritionalProfileController::class, 'show']);
     Route::post('perfil-nutricional', [NutritionalProfileController::class, 'store']);
     Route::put('perfil-nutricional/{nutritionalProfile}', [NutritionalProfileController::class, 'update']);
