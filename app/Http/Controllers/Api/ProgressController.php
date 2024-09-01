c9731a58f6a56246b3030144f3bc9dcb498a2c1a
Benjamín Campos <benjamin.camposc@usm.cl>
2024-09-01 15:22:53 -0400

    UPDATE show progress

--- app/Http/Controllers/Api/ProgressController.php
+++ app/Http/Controllers/Api/ProgressController.php
@@ -5,7 +5,11 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProgressRequest;
 use App\Http\Requests\UpdateProgressRequest;
+use App\Http\Resources\ProgressResource;
+use App\Models\Patient;
 use App\Models\Progress;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class ProgressController extends Controller
 {
@@ -28,9 +32,15 @@ class ProgressController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Progress $progress)
+    public function show(User $patient)
     {
-        //
+        $patient_nutri = Patient::where('nutritionist_id', Auth::id())->where('patient_id', $patient->id)->first();
+        if (!$patient_nutri) {
+            return response()->json(['message' => 'No tiene permisos para ver este paciente'], 403);
+        }
+        $progress = Progress::where('patient_id', $patient->id)->get();
+        return new ProgressResource($progress);
+
     }
 
     /**
