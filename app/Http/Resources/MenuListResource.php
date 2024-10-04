8cfc3b98dcea354541578f25f41a9172968cfba9
Benjamín Campos <benjamin.camposc@usm.cl>
2024-10-03 23:54:07 -0300

    UPDATE menu list resource

--- app/Http/Resources/MenuListResource.php
+++ app/Http/Resources/MenuListResource.php
@@ -14,14 +14,23 @@ class MenuListResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        if ($this->timespan == 7){
+            $type = "semanal";
+        } elseif ($this->whereBetween('timespan', [28, 31])){
+            $type = "mensual";
+        }
         return [
+            "_id" => $this->_id,
             'name' => $this->name,
             'user' => $this->user->name . " " . $this->user->last_name,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
             'list' => [
                 "diario" => $this->recipes,
                 "sm" => [
                     'timespan' => $this->timespan,
                     'menus' => $this->menus,
+                    'type'  => $type,
                 ]
             ]
         ];
