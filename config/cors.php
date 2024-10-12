6030a8dcae83b0a4d3f799c6f2eb378b6973a91c
Benjamín Campos <benjamin.camposc@usm.cl>
2024-10-11 23:12:51 -0300

    UPDATE cors por ahora

--- config/cors.php
+++ config/cors.php
@@ -21,10 +21,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        env('MOBILE_URL', 'http://localhost:8100'),
-        env('FRONTEND_URL', 'http://localhost:5174')
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
