34240de198bf18c5556180df58d8b8efc6dc403e
Benjamín Campos <benjamin.camposc@usm.cl>
2024-10-22 22:47:53 -0300

    FIX user relation with nutritional plan

--- app/Models/User.php
+++ app/Models/User.php
@@ -97,7 +97,12 @@ class User extends Authenticatable
 
     public function nutritionalPlan()
     {
-        return $this->hasMany(NutritionalPlan::class, 'patient_id')->withTrashed();
+        return $this->hasOne(NutritionalPlan::class, 'patient_id');
+    }
+
+    public function filedNutritionalPlan()
+    {
+        return $this->hasMany(NutritionalPlan::class, 'patient_id')->onlyTrashed();
     }
 
     public function recipes()
