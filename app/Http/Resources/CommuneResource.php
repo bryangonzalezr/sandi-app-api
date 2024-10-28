8e3e684960641f6725dbb83b549c8fe473173734
Benjamín Campos <benjamin.camposc@usm.cl>
2024-10-28 18:52:20 -0300

    UPDATE se rellenan resources

--- app/Http/Resources/CommuneResource.php
+++ app/Http/Resources/CommuneResource.php
@@ -12,7 +12,8 @@ class CommuneResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-
+            'id'   => $this->id,
+            'name' => $this->name,
         ];
     }
 }
