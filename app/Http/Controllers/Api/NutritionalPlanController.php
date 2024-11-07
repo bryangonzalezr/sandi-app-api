b1b39abbfa6cb05ed4ec0e4fb63b464f29f1ecd4
Benjamín Campos <benjamin.camposc@usm.cl>
2024-11-07 06:02:01 -0300

    FIX trashed plans

--- app/Http/Controllers/Api/NutritionalPlanController.php
+++ app/Http/Controllers/Api/NutritionalPlanController.php
@@ -77,7 +77,7 @@ class NutritionalPlanController extends Controller
      */
     public function show(User $patient)
     {
-        $nutritionalPlan = $patient->nutritionalPlan->withTrashed();
+        $nutritionalPlan = $patient?->nutritionalPlan->withTrashed();
         $return = $nutritionalPlan ? new NutritionalPlanResource($nutritionalPlan) :
                     response()->json(["data" => null]);
         return $return;
